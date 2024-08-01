741008d793973d7e82f48fc008f5904ba1126220
Volkor.316 <julio_c1994p@hotmail.com>
2024-08-01 09:23:33 -0700

    Mi primer commit

---
+++ configuration.py
@@ -0,0 +1,6 @@
+URL_SERVICE = "https://cnt-9f852a20-2b23-49b6-9dd6-be9fa84756ee.containerhub.tripleten-services.com"
+DOC_PATH = "/docs/"
+LOG_MAIN_PATH = "/api/logs/main/"
+USERS_TABLE_PATH = "/api/db/resources/user_model.csv"
+CREATE_USER_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
---
+++ create_user_test.py
@@ -0,0 +1,15 @@
+import sender_stand_request
+import data
+
+
+def get_user_body(first_name):
+    current_body = data.user_body.copy()
+    current_body["firstName"] = first_name
+    return current_body
+
+
+def test_create_user_number_type_first_name_get_error_response():
+    user_body = get_user_body(12)
+    response = sender_stand_request.post_new_user(user_body)
+
+    assert response.status_code == 400
